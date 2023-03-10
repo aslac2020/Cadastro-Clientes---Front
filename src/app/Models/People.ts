6d8fe5654d11083f01f71521e55f3c435e900163
André Lacerda <andredesenvolvedorti@gmail.com>
2023-03-10 07:26:18 -0300

    Implementação front-end

---
+++ src/app/Models/People.ts
@@ -0,0 +1,14 @@
+export class People {
+  id?: number;
+  nome!: string;
+  sobrenome!: string;
+  cpf!: string;
+  cep!: string;
+  nacionalidade?: string;
+  estado?: string;
+  logradouro?: string;
+  cidade?: string;
+  email!: string;
+  telefone!: string;
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home/home.component';
+import {MaterialModule} from "./shared/material/material.module";
+import {MatTableDataSource} from "@angular/material/table";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+
+}
